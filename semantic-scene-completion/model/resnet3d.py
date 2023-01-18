fix iou calculation

semantic-scene-completion/model/resnet3d.py
@@ -1,4 +1,5 @@
 from torch import nn
+from configs import config
 
 
 class ResNetBlock3d(nn.Module):
@@ -8,9 +9,11 @@ class ResNetBlock3d(nn.Module):
         self.conv1 = nn.Conv3d(in_features, out_features, 3, stride, 1, bias=False)
         self.bn1 = nn.InstanceNorm3d(out_features)
         self.relu = nn.ReLU(inplace=True)
+        self.dropout1 = nn.Dropout(p=config.TRAIN.DROPOUT)
 
         self.conv2 = nn.Conv3d(out_features, out_features, 3, 1, 1, bias=False)
         self.bn2 = nn.InstanceNorm3d(out_features)
+        self.dropout2 = nn.Dropout(p=config.TRAIN.DROPOUT)
 
         self.downsample = downsample
 
@@ -20,6 +23,7 @@ class ResNetBlock3d(nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
+        out = self.dropout1(out)
 
         out = self.conv2(out)
         out = self.bn2(out)
@@ -29,6 +33,7 @@ class ResNetBlock3d(nn.Module):
 
         out += identity
         out = self.relu(out)
+        out = self.dropout2(out)
 
         return out
 
@@ -51,9 +56,11 @@ class ResNetBlockTranspose3d(nn.Module):
         self.conv1 = nn.ConvTranspose3d(in_features, out_features, 3, stride, 1, 1, bias=False)
         self.bn1 = nn.InstanceNorm3d(out_features)
         self.relu = nn.ReLU(inplace=True)
+        self.dropout1 = nn.Dropout(p=config.TRAIN.DROPOUT)
 
         self.conv2 = nn.Conv3d(out_features, out_features, 3, 1, 1, bias=False)
         self.bn2 = nn.InstanceNorm3d(out_features)
+        self.dropout2 = nn.Dropout(p=config.TRAIN.DROPOUT)
 
         self.upsample = upsample
 
@@ -63,15 +70,18 @@ class ResNetBlockTranspose3d(nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
+        # out = self.dropout1(out)
 
         out = self.conv2(out)
         out = self.bn2(out)
+
 
         if self.upsample is not None:
             identity = self.upsample(x)
 
         out += identity
         out = self.relu(out)
+        # out = self.dropout2(out)
 
         return out
 
@@ -83,97 +93,3 @@ class ResNetTranspose(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
-
-
-# from torch import nn
-# from configs import config
-
-
-# class ResNetBlock3d(nn.Module):
-#     def __init__(self, in_features, out_features, stride=1, downsample=None):
-#         super().__init__()
-
-#         self.conv1 = nn.Conv3d(in_features, out_features, 3, stride, 1, bias=False)
-#         self.bn1 = nn.InstanceNorm3d(out_features)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.dropout1 = nn.Dropout(p=config.TRAIN.DROPOUT)
-
-#         self.conv2 = nn.Conv3d(out_features, out_features, 3, 1, 1, bias=False)
-#         self.bn2 = nn.InstanceNorm3d(out_features)
-#         self.dropout2 = nn.Dropout(p=config.TRAIN.DROPOUT)
-
-
-#         self.downsample = downsample
-
-#     def forward(self, x):
-#         identity = x
-
-#         out = self.conv1(x)
-#         out = self.bn1(out)
-#         out = self.relu(out)
-#         out = self.dropout1(out)
-
-#         out = self.conv2(out)
-#         out = self.bn2(out)
-#         out = self.dropout2(out)
-
-#         if self.downsample is not None:
-#             identity = self.downsample(x)
-
-#         out += identity
-#         out = self.relu(out)
-
-#         return out
-
-
-# class ResNetDownsample(nn.Module):
-#     def __init__(self, in_features, out_features, stride=1):
-#         super().__init__()
-#         self.conv = nn.Conv3d(in_features, out_features, 1, stride, bias=False)
-#         self.norm = nn.InstanceNorm3d(out_features)
-
-#     def forward(self, x):
-#         return self.norm(self.conv(x))
-
-
-# class ResNetBlockTranspose3d(nn.Module):
-#     def __init__(self, in_features, out_features, stride=1, upsample=None):
-#         super().__init__()
-
-#         self.conv1 = nn.ConvTranspose3d(in_features, out_features, 3, stride, 1, 1, bias=False)
-#         self.bn1 = nn.InstanceNorm3d(out_features)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.dropout1 = nn.Dropout(p=config.TRAIN.DROPOUT)
-
-#         self.conv2 = nn.Conv3d(out_features, out_features, 3, 1, 1, bias=False)
-#         self.bn2 = nn.InstanceNorm3d(out_features)
-#         self.dropout2 = nn.Dropout(p=config.TRAIN.DROPOUT)
-
-#         self.upsample = upsample
-
-#     def forward(self, x):
-#         identity = x
-
-#         out = self.conv1(x)
-#         out = self.bn1(out)
-#         out = self.relu(out)
-
-#         out = self.conv2(out)
-#         out = self.bn2(out)
-
-#         if self.upsample is not None:
-#             identity = self.upsample(x)
-
-#         out += identity
-#         out = self.relu(out)
-
-#         return out
-
-
-# class ResNetTranspose(nn.Module):
-#     def __init__(self, in_features, out_features, stride=1):
-#         super().__init__()
-#         self.conv = nn.ConvTranspose3d(in_features, out_features, 3, stride, 1, 1, bias=False)
-
-#     def forward(self, x):
-#         return self.conv(x)

semantic-scene-completion/model/resnet_sparse.py
@@ -1,6 +1,6 @@
 from torch import nn
 import MinkowskiEngine as Me
-
+from configs import config
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -19,9 +19,11 @@ class BasicBlock(nn.Module):
         self.conv1 = Me.MinkowskiConvolution(
             inplanes, planes, kernel_size=3, stride=stride, dilation=dilation, dimension=dimension)
         self.norm1 = Me.MinkowskiInstanceNorm(planes)
+        self.dropout1 = Me.MinkowskiDropout(p=config.TRAIN.DROPOUT)
         self.conv2 = Me.MinkowskiConvolution(
             planes, planes, kernel_size=3, stride=1, dilation=dilation, dimension=dimension)
         self.norm2 = Me.MinkowskiInstanceNorm(planes)
+        self.dropout2 = Me.MinkowskiDropout(p=config.TRAIN.DROPOUT)
         self.relu = Me.MinkowskiReLU(inplace=True)
         self.downsample = downsample
 
@@ -31,6 +33,7 @@ class BasicBlock(nn.Module):
         out = self.conv1(x)
         out = self.norm1(out)
         out = self.relu(out)
+        out = self.dropout1(out)
 
         out = self.conv2(out)
         out = self.norm2(out)
@@ -40,6 +43,7 @@ class BasicBlock(nn.Module):
 
         out += residual
         out = self.relu(out)
+        out = self.dropout2(out)
 
         return out
 

semantic-scene-completion/semantic_kitti_dataset.py
@@ -177,7 +177,7 @@ class SemanticKITTIDataset(Dataset):
             rot_zyx=[0,0,0]
         completion_collection['flip_mode'] = flip_mode
         # flip_mode = 1
-        # rot_zyx=[10,5,0]
+        # rot_zyx=[10,5,1]
 
         # read raw data and unpack (if necessary)
         for typ in self.files.keys():
@@ -275,11 +275,11 @@ class SemanticKITTIDataset(Dataset):
             # Inverse centering translation
             locs_aug[:,1] += 256./2.0
             # Add translations
-            if self.config.TRAIN.RANDOM_TRANSLATION:
-                translation = torch.randint(-1,2,locs_aug.shape)
-                mask = torch.rand(translation.shape) < 0.75
-                translation[mask] = 0
-                locs_aug+=translation
+            translation = torch.randint(-1,2,locs_aug.shape)
+            mask = torch.rand(translation.shape) < (1.0 - self.config.TRAIN.RANDOM_TRANSLATION_PROB)
+            translation[mask] = 0
+            locs_aug+=translation
+            
             # check inside bounds
             valid = torch.where((locs_aug[:,0]>=0) & (locs_aug[:,1]>=0 ) & (locs_aug[:,2]>=0) & (locs_aug[:,0]<=255) & (locs_aug[:,1]<=255) & (locs_aug[:,2]<=31))
             locs_aug = locs_aug[valid[0]]
@@ -331,7 +331,6 @@ class SemanticKITTIDataset(Dataset):
 
     def data_augmentation(self, t, flip_mode, rot_zyx=[0,0,0], level=64, inp = False, inverse=False):
         rot_zyx_flipped =  rot_zyx.copy()# TODO (juan.galvis): correction for unwanted flip
-
         assert t.dim() == 4, 'input dimension should be 4!'
         # FLIP (we only flip the 50% of the times)
         if flip_mode == 1:
@@ -496,7 +495,7 @@ def Merge(tbl):
     invalid_locs = torch.where(complet_labels > 255)
     complet_labels[invalid_locs] = 255
     # complet_occupancy = torch.where(torch.logical_and(complet_labels > 0, complet_labels < 255), one, zero) # TODO: is invalid occupied or unoccupied?
-    complet_occupancy = torch.where(torch.logical_and(complet_labels > 0, complet_labels < 255), one, zero) # TODO: is invalid occupied or unoccupied?
+    complet_occupancy = torch.where(complet_labels > 0  , one, zero)
 
     
     # complet_labels_128 = F.max_pool3d(complet_labels.float(), kernel_size=2, stride=2).int()
@@ -504,8 +503,8 @@ def Merge(tbl):
     complet_labels_128[0, invalid_locs_128[:, 0], invalid_locs_128[:, 1], invalid_locs_128[:, 2]] = 255
     invalid_locs = torch.where(complet_labels_128 > 255)
     complet_labels_128[invalid_locs] = 255
-    # complet_occupancy_128 = torch.where(complet_labels_128 > 0  , one, zero)
-    complet_occupancy_128 = torch.where(torch.logical_and(complet_labels_128 > 0, complet_labels_128 < 255), one, zero) # TODO: is invalid occupied or unoccupied?
+    complet_occupancy_128 = torch.where(complet_labels_128 > 0  , one, zero)
+    # complet_occupancy_128 = torch.where(torch.logical_and(complet_labels_128 > 0, complet_labels_128 < 255), one, zero) # TODO: is invalid occupied or unoccupied?
 
 
     # complet_labels_64 = F.max_pool3d(complet_labels_128.float(), kernel_size=2, stride=2).int()
@@ -513,8 +512,8 @@ def Merge(tbl):
     complet_labels_64[0, invalid_locs_64[:, 0], invalid_locs_64[:, 1], invalid_locs_64[:, 2]] = 255
     invalid_locs = torch.where(complet_labels_64 > 255)
     complet_labels_64[invalid_locs] = 255
-    # complet_occupancy_64 = torch.where(complet_labels_64 > 0, one, zero)
-    complet_occupancy_64 = torch.where(torch.logical_and(complet_labels_64 > 0, complet_labels_64 < 255), one, zero) # TODO: is invalid occupied or unoccupied?
+    complet_occupancy_64 = torch.where(complet_labels_64 > 0, one, zero)
+    # complet_occupancy_64 = torch.where(torch.logical_and(complet_labels_64 > 0, complet_labels_64 < 255), one, zero) # TODO: is invalid occupied or unoccupied?
 
 
 

semantic-scene-completion/train.py
@@ -105,9 +105,9 @@ def main():
             model.eval()
             with torch.no_grad():
                 for dataloader_name, dataloader in val_dataloaders.items():
-                    seg_evaluators = {"64": iouEval(config.SEGMENTATION.NUM_CLASSES, []),
-                                      "128": iouEval(config.SEGMENTATION.NUM_CLASSES, []),  
-                                      "256": iouEval(config.SEGMENTATION.NUM_CLASSES, []),}
+                    seg_evaluators = {"64": iouEval(config.SEGMENTATION.NUM_CLASSES, [0]),
+                                      "128": iouEval(config.SEGMENTATION.NUM_CLASSES, [0]),  
+                                      "256": iouEval(config.SEGMENTATION.NUM_CLASSES, [0]),}
 
                     occ_evaluators = {"64": iouEval(2, []),
                                         "128": iouEval(2, []),  
@@ -171,12 +171,8 @@ def main():
                         # del complet_inputs, results, semantic_labels, semantic_gt, prediction, predicted_coordinates
                     for level in levels:
                         print("\nEvaluating level: {}".format(level))
-                        _, occ_iou = occ_evaluators[level].getIoU()
-                        occ_miou = occ_iou.mean()
-                        # print("mean_occ_iou: ", occ_miou)
-                        _, class_jaccard = seg_evaluators[level].getIoU()
-                        m_jaccard = class_jaccard.mean()
-                        # print("mean_iou: ", m_jaccard)
+                        occ_miou, occ_iou = occ_evaluators[level].getIoU()
+                        m_jaccard, class_jaccard = seg_evaluators[level].getIoU()
                         ignore = []
                         for i, jacc in enumerate(class_jaccard):
                             if i not in ignore: