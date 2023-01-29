fix calculation of tensors for ioU evaluation in training

semantic-scene-completion/model/my_model.py
@@ -139,6 +139,7 @@ class MyModel(nn.Module):
 
     def compute_semantic_256_loss(self, prediction: Me.SparseTensor, ground_truth: torch.Tensor, weights: torch.Tensor) -> Tuple[Dict, Dict]:
         prediction = mask_invalid_sparse_voxels(prediction)
+        
         predicted_coordinates = prediction.C.long()
         # predicted_coordinates[:, 1:] = torch.div(predicted_coordinates[:, 1:], prediction.tensor_stride[0], rounding_mode="floor")
         predicted_coordinates[:, 1:] = predicted_coordinates[:, 1:] // prediction.tensor_stride[0]
@@ -468,7 +469,7 @@ class MyModel(nn.Module):
         results["occupancy_256"] = occupancy_prediction
         # level-FULL
         if config.GENERAL.LEVEL == "FULL":
-            occupancy_mask = (occupancy_prediction.F > 0.5).squeeze()
+            occupancy_mask = (occupancy_prediction.F > 0.5).squeeze() # TODO: Find a better threshold
             prediction_pruned = Me.MinkowskiPruning()(feature_prediction, occupancy_mask)
             # print("prediction_pruned: ", prediction_pruned.shape)
 

semantic-scene-completion/semantic_kitti_dataset.py
@@ -513,8 +513,8 @@ def Merge(tbl):
     complet_labels[0,invalid_locs[:,0], invalid_locs[:,1], invalid_locs[:,2]] = 255
     invalid_locs = torch.where(complet_labels > 255)
     complet_labels[invalid_locs] = 255
-    complet_occupancy = torch.where(torch.logical_and(complet_labels > 0, complet_labels < 255), one, zero) # TODO: is invalid occupied or unoccupied?
-    # complet_occupancy = torch.where(complet_labels > 0  , one, zero)
+    # complet_occupancy = torch.where(torch.logical_and(complet_labels > 0, complet_labels < 255), one, zero) # TODO: is invalid occupied or unoccupied?
+    complet_occupancy = torch.where(complet_labels > 0  , one, zero)
 
     
     # complet_labels_128 = F.max_pool3d(complet_labels.float(), kernel_size=2, stride=2).int()
@@ -522,8 +522,8 @@ def Merge(tbl):
     complet_labels_128[0, invalid_locs_128[:, 0], invalid_locs_128[:, 1], invalid_locs_128[:, 2]] = 255
     invalid_locs = torch.where(complet_labels_128 > 255)
     complet_labels_128[invalid_locs] = 255
-    # complet_occupancy_128 = torch.where(complet_labels_128 > 0  , one, zero)
-    complet_occupancy_128 = torch.where(torch.logical_and(complet_labels_128 > 0, complet_labels_128 < 255), one, zero) # TODO: is invalid occupied or unoccupied?
+    complet_occupancy_128 = torch.where(complet_labels_128 > 0  , one, zero)
+    # complet_occupancy_128 = torch.where(torch.logical_and(complet_labels_128 > 0, complet_labels_128 < 255), one, zero) # TODO: is invalid occupied or unoccupied?
 
 
     # complet_labels_64 = F.max_pool3d(complet_labels_128.float(), kernel_size=2, stride=2).int()
@@ -531,8 +531,8 @@ def Merge(tbl):
     complet_labels_64[0, invalid_locs_64[:, 0], invalid_locs_64[:, 1], invalid_locs_64[:, 2]] = 255
     invalid_locs = torch.where(complet_labels_64 > 255)
     complet_labels_64[invalid_locs] = 255
-    # complet_occupancy_64 = torch.where(complet_labels_64 > 0, one, zero)
-    complet_occupancy_64 = torch.where(torch.logical_and(complet_labels_64 > 0, complet_labels_64 < 255), one, zero) # TODO: is invalid occupied or unoccupied?
+    complet_occupancy_64 = torch.where(complet_labels_64 > 0, one, zero)
+    # complet_occupancy_64 = torch.where(torch.logical_and(complet_labels_64 > 0, complet_labels_64 < 255), one, zero) # TODO: is invalid occupied or unoccupied?
 
 
 

semantic-scene-completion/test.py
@@ -1,7 +1,7 @@
 import torch
 from configs import config
 import argparse
-from semantic_kitti_dataset import SemanticKITTIDataset, MergeTest
+from semantic_kitti_dataset import SemanticKITTIDataset, MergeTest, Merge
 import numpy as np
 import time
 from tqdm import tqdm
@@ -23,11 +23,11 @@ shapes = {"256": torch.Size([1, 1, 256, 256, 32]), "128": torch.Size([1, 1, 128,
 
 def main():
     re_seed(0)
-    test_dataset = SemanticKITTIDataset(config, "test",do_overfit=False, augment=False)
+    test_dataset = SemanticKITTIDataset(config, "valid",do_overfit=False, augment=False)
     test_dataloader = torch.utils.data.DataLoader(
         test_dataset,
         batch_size=config.TRAIN.BATCH_SIZE,
-        collate_fn=MergeTest,
+        collate_fn=Merge,
         num_workers=config.TRAIN.NUM_WORKERS,
         pin_memory=True,
         shuffle=False,
@@ -37,7 +37,7 @@ def main():
 
     model = MyModel(num_output_channels=config.MODEL.NUM_OUTPUT_CHANNELS, unet_features=config.MODEL.UNET_FEATURES)
 
-    ckpt_path = "experiments/177/modelFULL-90.pth" #config.GENERAL.CHECKPOINT_PATH
+    ckpt_path = "experiments/173/modelFULL-60.pth" #config.GENERAL.CHECKPOINT_PATH
 
     try:
         model.load_state_dict(torch.load(ckpt_path))
@@ -85,6 +85,8 @@ def main():
             occupancy_prediction = results['occupancy_{}'.format(level)]
             semantic_prediction = results['semantic_labels_{}'.format(level)]
 
+            
+
             shape = shapes[level]
             min_coordinate = torch.IntTensor([0, 0, 0]).to(device)
 
@@ -98,6 +100,13 @@ def main():
                 occupancy_prediction = np.uint16(occupancy_prediction[0,0].detach().cpu().numpy())
                 semantic_prediction, _, _ = semantic_prediction.dense(shape, min_coordinate=min_coordinate)
                 semantic_prediction = np.uint16(semantic_prediction.to("cpu")[0,0].detach().cpu().numpy())
+
+            # Override prediction 
+            # print("semantic_prediction.shape: ", semantic_prediction.shape)
+            # gt_labels = collect(complet_inputs, "complet_labels_{}".format(level)).squeeze()
+            # semantic_prediction = np.uint16(gt_labels.detach().cpu().numpy())
+            # semantic_prediction[semantic_prediction==255] = 0
+            # print("gt.shape: ", semantic_prediction.shape)
             # remap
             # TODO: Do Something with the results -> save, etc
             maxkey = max(remapdict_inv.keys())
@@ -105,7 +114,16 @@ def main():
             remap_lut[list(remapdict_inv.keys())] = list(remapdict_inv.values())
             semantic_prediction = remap_lut[semantic_prediction]
             semantic_prediction = np.uint16(semantic_prediction)
-            filename = "output/sequences/{}/{}.label".format(filenames[0][0],filenames[0][1])
+            try:
+                filename = "output/valid/sequences/{}/predictions/{}.label".format(filenames[0][0],filenames[0][1])
+            except:
+                new_dir = "output/valid/sequences/{}/predictions".format(filenames[0][0])
+                print("creating file: ", new_dir)
+                try:
+                    os.mkdir(new_dir)
+                except OSError as error:
+                    print(error)  
+                continue
             semantic_prediction.astype('int16').tofile(filename)
             log_msg = {"sequence": filenames[0][0], "sample": filenames[0][1]}
             pbar.set_postfix(log_msg)

semantic-scene-completion/train.py
@@ -18,6 +18,7 @@ import nvidia_smi
 
 
 device = torch.device("cuda:0")
+shapes = {"256": torch.Size([1, 1, 256, 256, 32]), "128": torch.Size([1, 1, 128, 128, 16]), "64": torch.Size([1, 1, 64, 64, 8])}
 
 def main():
     re_seed(0)
@@ -101,13 +102,22 @@ def main():
     print("Total trainable params: ", pytorch_total_trainable_params)
     for epoch in range(training_epoch, (config.TRAIN.MAX_EPOCHS)):
         # ============== Evaluation ==============
+        if epoch>=steps_schedule[0]:
+            if epoch>=steps_schedule[1]:
+                if epoch>=steps_schedule[2]:
+                    config.GENERAL.LEVEL = "FULL"
+                else:
+                    config.GENERAL.LEVEL = "256"
+            else:
+                config.GENERAL.LEVEL = "128"
+                
         if epoch % config.TRAIN.EVAL_PERIOD == 0 and config.GENERAL.LEVEL != "256":
             model.eval()
             with torch.no_grad():
                 for dataloader_name, dataloader in val_dataloaders.items():
-                    seg_evaluators = {"64": iouEval(config.SEGMENTATION.NUM_CLASSES, [0]),
-                                      "128": iouEval(config.SEGMENTATION.NUM_CLASSES, [0]),  
-                                      "256": iouEval(config.SEGMENTATION.NUM_CLASSES, [0]),}
+                    seg_evaluators = {"64": iouEval(config.SEGMENTATION.NUM_CLASSES, []),
+                                      "128": iouEval(config.SEGMENTATION.NUM_CLASSES, []),  
+                                      "256": iouEval(config.SEGMENTATION.NUM_CLASSES, []),}
 
                     occ_evaluators = {"64": iouEval(2, []),
                                         "128": iouEval(2, []),  
@@ -117,9 +127,9 @@ def main():
                     if config.GENERAL.LEVEL == "64":
                         levels = ["64"]
                     elif config.GENERAL.LEVEL == "128":
-                        levels = ["64", "128"]
+                        levels = ["128"] # ["64", "128"]
                     elif config.GENERAL.LEVEL == "FULL":
-                        levels = ["64", "128", "256"]
+                        levels = ["256"] #["64", "128", "256"]
                     for i, batch in enumerate(tqdm(dataloader)):
                         _, complet_inputs, _, _ = batch
                         # try:
@@ -148,32 +158,32 @@ def main():
                                 semantic_labels = semantic_labels[semantic_gt!=255]
                                 seg_evaluators[level].addBatch(semantic_labels.astype(int), seg_labels_gt.astype(int))
                             else:
-                                occupancy_prediction = results['occupancy_{}'.format(level)]
-                                occupancy_coordinates = occupancy_prediction.C.long()
-                                occupancy_coordinates[:, 1:] = occupancy_coordinates[:, 1:] // occupancy_prediction.tensor_stride[0]
-                                occupancy_gt = get_sparse_values(occupancy_gt.unsqueeze(0), occupancy_coordinates)
-                                occupancy_prediction = occupancy_prediction.F > 0.5
-                                occupancy_gt = occupancy_gt[:,0].to('cpu').data.numpy()
-                                occupancy_prediction = occupancy_prediction[:,0].to('cpu').data.numpy()
-                                occ_evaluators[level].addBatch(occupancy_prediction.astype(int), occupancy_gt.astype(int))
-
-                                prediction = results['semantic_prediction_{}'.format(level)]
-                                predicted_coordinates = prediction.C.long()
-                                predicted_coordinates[:, 1:] = predicted_coordinates[:, 1:] // prediction.tensor_stride[0]
-                                semantic_gt = get_sparse_values(semantic_gt.unsqueeze(0), predicted_coordinates)
-                                semantic_labels = semantic_labels.F
-                                torch.cuda.empty_cache()
-                                semantic_gt = semantic_gt[:,0].to('cpu').data.numpy()
-                                semantic_labels = semantic_labels[:,0].to('cpu').data.numpy()
-                                seg_labels_gt = semantic_gt[semantic_gt!=255]
+                                min_coordinate = torch.IntTensor([0, 0, 0]).to(device)
+                                shape = shapes[level]
+                                # prediction = results['semantic_prediction_{}'.format(level)]
+                                semantic_labels, _, _ = semantic_labels.dense(shape, min_coordinate=min_coordinate)
+                                semantic_labels = np.uint16(semantic_labels.to("cpu")[0,0].detach().cpu().numpy()).flatten()
+                                semantic_gt = np.uint16(semantic_gt.detach().cpu().numpy()).flatten()
                                 semantic_labels = semantic_labels[semantic_gt!=255]
-                                seg_evaluators[level].addBatch(semantic_labels.astype(int), seg_labels_gt.astype(int))
+                                
+                                
+                                occupancy_prediction = results['occupancy_{}'.format(level)]
+                                occupancy_prediction, _, _ = occupancy_prediction.dense(shape, min_coordinate=min_coordinate)
+                                occupancy_prediction = np.uint16(occupancy_prediction.to("cpu")[0,0].detach().cpu().numpy()).flatten()
+                                occupancy_gt = np.uint16(occupancy_gt.detach().cpu().numpy()).flatten()
+                                occupancy_gt = occupancy_gt[semantic_gt!=255]
+                                occupancy_prediction = occupancy_prediction[semantic_gt!=255]
+                                semantic_gt = semantic_gt[semantic_gt!=255]
+                                torch.cuda.empty_cache()
+                                occ_evaluators[level].addBatch(occupancy_prediction.astype(int), occupancy_gt.astype(int))
+                                seg_evaluators[level].addBatch(semantic_labels.astype(int), semantic_gt.astype(int))
                         # del complet_inputs, results, semantic_labels, semantic_gt, prediction, predicted_coordinates
                     for level in levels:
                         print("\nEvaluating level: {}".format(level))
                         occ_miou, occ_iou = occ_evaluators[level].getIoU()
-                        m_jaccard, class_jaccard = seg_evaluators[level].getIoU()
-                        ignore = []
+                        _, class_jaccard = seg_evaluators[level].getIoU()
+                        m_jaccard = class_jaccard[1:].mean()
+                        ignore = [0]
                         for i, jacc in enumerate(class_jaccard):
                             if i not in ignore:
                                 writers[dataloader_name].add_scalar('eval-{}/{}'.format(level,seg_label_to_cat[i]), jacc*100, epoch)
@@ -186,14 +196,6 @@ def main():
                     torch.cuda.empty_cache()
 
         # # ============== Training ==============
-        if epoch>=steps_schedule[0]:
-            if epoch>=steps_schedule[1]:
-                if epoch>=steps_schedule[2]:
-                    config.GENERAL.LEVEL = "FULL"
-                else:
-                    config.GENERAL.LEVEL = "256"
-            else:
-                config.GENERAL.LEVEL = "128"
         
         '''Adjust learning rate'''
         lr = max(config.SOLVER.BASE_LR * (config.SOLVER.LR_DECAY** (epoch // config.SOLVER.DECAY_STEP)), config.SOLVER.LR_CLIP)